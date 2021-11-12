95ff95ee252aa5834d2428c57c343dfcf48052b3
Kirill Vashchenko <k.vashhenko@gmail.com>
2021-11-12 09:29:32 +0300

    fix text

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,19 +6,19 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = (("Список команд: ",
-             "/start - Начать диалог",
-             '/register - начать регистрацию',
-             "/help - Получить справку"))
+    text = ("Список команд: ",
+            "/start - Начать диалог",
+            '/register - начать регистрацию',
+            "/help - Получить справку")
 
     await message.answer("\n".join(text))
 
 
 @dp.message_handler(Command('help_admin'))
 async def bot_help_admin(message: types.Message):
-    text = (("Список команд: ",
-             "/all_register_users - Показать всех зарегестрированных пользователей",
-             '/drop_all_users - Отлистить базу',
-             "/help - Получить справку"))
+    text = ("Список команд: ",
+            "/all_register_users - Показать всех зарегестрированных пользователей",
+            '/drop_all_users - Отлистить базу',
+            "/help - Получить справку")
 
     await message.answer("\n".join(text))
